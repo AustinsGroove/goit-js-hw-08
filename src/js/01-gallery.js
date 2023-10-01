f354a95b6d05bb2e5207c201e205729a1a12e2f6
AustinsGroove <vadimgajiy@gmail.com>
2023-10-01 19:45:12 +0300

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('ul.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.style.listStyle = 'none';
+
+const lightbox = new SimpleLightbox('ul.gallery .gallery__link', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
