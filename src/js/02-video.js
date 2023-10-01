a1c2933ed37795d24657a2943d875ff03f19278e
AustinsGroove <vadimgajiy@gmail.com>
2023-10-02 00:45:36 +0300

    5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+if (localStorage['videoplayer-current-time']) {
+  player.setCurrentTime(
+    JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  );
+}
+
+player.on('timeupdate', throttle(timeUpdateHandler, 1000));
+
+function timeUpdateHandler(event) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(event.seconds)
+  );
+  console.log(localStorage.getItem('videoplayer-current-time'));
+}
