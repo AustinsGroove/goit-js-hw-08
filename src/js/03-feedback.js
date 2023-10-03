17589652ec999c0dbb114c23f5192dcb87df5e09
AustinsGroove <vadimgajiy@gmail.com>
2023-10-03 22:16:48 +0300

    8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,15 @@
 const feedbackForm = document.querySelector('.feedback-form');
 const throttle = require('lodash.throttle');
+const STORAGE_KEY = 'feedback-form-state';
 
+const { email, message } = feedbackForm.elements;
 let feedbackFormState = {};
 
-if (localStorage['feedback-form-state']) {
+if (localStorage[STORAGE_KEY]) {
   try {
-    feedbackFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
-    feedbackForm.elements.email.value = feedbackFormState.email;
-    feedbackForm.elements.message.value = feedbackFormState.message;
+    feedbackFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    email.value = feedbackFormState.email;
+    message.value = feedbackFormState.message;
   } catch (error) {
     console.log(error.name);
     console.log(error.message);
@@ -18,20 +20,21 @@ feedbackForm.addEventListener('input', throttle(inputHandler, 500));
 feedbackForm.addEventListener('submit', submitHandler);
 
 function inputHandler() {
-  feedbackFormState.email = feedbackForm.elements.email.value;
-  feedbackFormState.message = feedbackForm.elements.message.value;
-  localStorage.setItem(
-    'feedback-form-state',
-    JSON.stringify(feedbackFormState)
-  );
-  //   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  feedbackFormState.email = email.value;
+  feedbackFormState.message = message.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+  // console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
 
 function submitHandler(event) {
   event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    alert(`You must fill out all fields of the form!`);
+    return;
+  }
   console.log(feedbackFormState);
-  localStorage.removeItem('feedback-form-state');
-  feedbackForm.elements.email.value = '';
-  feedbackForm.elements.message.value = '';
+  localStorage.removeItem(STORAGE_KEY);
+  email.value = '';
+  message.value = '';
   feedbackFormState = {};
 }
